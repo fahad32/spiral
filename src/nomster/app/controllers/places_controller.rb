21866fcdd047d32e54ab0db11be3d483d06a86fb
Fahad Jamal <fmj213@stern.nyu.edu>
2015-07-10 16:25:43 +0000

    Updated navbar

--- src/nomster/app/controllers/places_controller.rb
+++ src/nomster/app/controllers/places_controller.rb
@@ -2,4 +2,32 @@ class PlacesController < ApplicationController
 	def index
 		@places = Place.all
 	end
+
+	def new
+		@place = Place.new
+    end
+
+    def create
+    Place.create(place_params)
+     redirect_to root_path
+    end
+
+    def destroy
+      @place = Place.find(params[:id])
+      @place.destroy
+      redirect_to root_path
+    end
+
+    def show
+      @place_something = Place.find(params[:id])
+
+    end
+
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
+
 end
